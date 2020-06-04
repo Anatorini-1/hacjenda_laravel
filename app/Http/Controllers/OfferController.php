893bcf29e263e49302d372518952cb7606ae4b76
Wojtazzs <michi.wojtasik@gmail.com>
2020-06-04 10:36:57 +0200

    1.1.6 Search zrobiony bez ceny bo zapomniałem

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -141,10 +141,15 @@ class OfferController extends Controller
         //$url_components = parse_url($url); 
         $search = request('search');
         $sort = request('sort');
-        if ($search == null) {
+        $praca = request('np');
+        if ($search == null && $praca== null) {
             $offers = Offer::where('stan', 'otwarta')->orderBy('created_at', $sort)->get();
-        } else {
+        }else if($praca == null){
             $offers = Offer::where('stan', 'otwarta')->where('miasto', 'LIKE', "%{$search}%")->orderBy('created_at', $sort)->get();
+        } else if($search == null){
+            $offers = Offer::where('stan', 'otwarta')->where('jobs', 'LIKE',  "%\"$praca\"%")->orderBy('created_at', $sort)->get();
+        }else{
+            $offers = Offer::where('stan', 'otwarta')->where('miasto', 'LIKE', "%{$search}%")->where('jobs', 'LIKE',  "%\"$praca\"%")->orderBy('created_at', $sort)->get();
         }
         $items_per_page = 6;
         $toDisplay = [];
@@ -160,12 +165,11 @@ class OfferController extends Controller
             }
         }
 
-
         return view('offers.index', [
             'data' => $toDisplay,
             'pageCount' => $pageCounter,
             'beforesearch' => 'search/',
-            'activesearch' => "?search=" . $search . "&sort=" . $sort,
+            'activesearch' => "?search=" . $search . "&sort=" . $sort . "&np=" . $praca,
             'activePage' => $active_page,
             'search' => $search,
             'sort' => $sort,
@@ -294,12 +298,17 @@ class OfferController extends Controller
         $items_per_page = 6;
         $search = request('search');
         $sort = request('sort');
+        $praca = request('praca');
         if($sort=="")
             $sort = 'desc';
-        if ($search == null) {
-            $offers = Offer::where('stan', 'zakonczona')->orderBy('created_at', $sort)->get();
-        } else {
-            $offers = Offer::where('stan', 'zakonczona')->where('miasto', 'LIKE', "%{$search}%")->orderBy('created_at', $sort)->get();
+        if ($search == null && $praca== null) {
+            $offers = Offer::where('stan', 'otwarta')->orderBy('created_at', $sort)->get();
+        }else if($praca == null){
+            $offers = Offer::where('stan', 'otwarta')->where('miasto', 'LIKE', "%{$search}%")->orderBy('created_at', $sort)->get();
+        }else if($search == null){
+            $offers = Offer::where('stan', 'otwarta')->where('jobs', 'LIKE',  "%\"$praca\"%")->orderBy('created_at', $sort)->get();
+        }else{
+            $offers = Offer::where('stan', 'otwarta')->where('miasto', 'LIKE', "%{$search}%")->where('jobs', 'LIKE',  "%\"$praca\"%")->orderBy('created_at', $sort)->get();
         }
         $toDisplay = [];
         $pageCounter = floor((sizeof($offers) / $items_per_page)) + 1;
--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,10 @@ var url = window.location.search.substr(1);
 var miasta = url.split('&');
 var miasto = "";
 var finished = location.href.indexOf('finished');
+var sitee = location.href;
+console.log(sitee);
+var site_url = 'http://127.0.0.1:8000/';
+var numer_praca = 0;
 function logkey(){
 document.addEventListener('keypress', pressed);
 }
@@ -14,29 +18,51 @@ function pressed(e){
 }
 function OrderAsc(){
     miasto = miasta[0].split('=')[1];
+    numer_praca = miasta[2].split('=')[1];
     sort = 'asc';
     Szukaj(miasto);
 }
 function OrderDesc(){
     miasto = miasta[0].split('=')[1];
+    numer_praca = miasta[2].split('=')[1];
     sort = 'desc'
     Szukaj(miasto);
 }
+
 function Szukaj(miasto){
-    if(miasto=="" || miasto==undefined){
+    var praca = document.querySelector('#praca-list').value;
+    if(numer_praca == 0){
+        if(praca == 'Mycie Okien' || praca == 'mycie okien' || praca == 'Mycie okien' || praca == 'mycie Okien')
+            numer_praca = 1;
+        if(praca == 'Mycie Auta' || praca == 'mycie Auta' || praca == 'Mycie auta' || praca == 'mycie auta')
+            numer_praca = 2;
+        if(praca == 'Odkurzanie' || praca == 'odkurzanie')
+            numer_praca = 3;
+        if(praca == 'Zcieranie kurzu' || praca == 'zcieranie kurzu' || praca == 'Zcieranie Kurzu' || praca == 'zcieranie Kurzu')
+            numer_praca = 4;
+        if(praca == 'Mycie Podłóg' || praca == 'mycie Podłóg' || praca == 'mycie podłóg' || praca == 'Mycie podłóg')
+            numer_praca = 5;
+        if(praca == 'Sprzątanie Łazienki' || praca == 'sprzątanie łazienki' || praca == 'Sprzątanie łazienki' || praca == 'sprzątanie Łazienki')
+            numer_praca = 6;
+    }
+    if(numer_praca != 0){
+        search = document.querySelector('#searchs').value;
+        window.location.replace(site_url + 'offers/search/1?search=' + search + '&sort=' + sort + '&np=' + numer_praca);
+        }
+    else if(miasto=="" || miasto==undefined){
         if(finished != -1){
-            search = document.querySelector('#searchtxt').value;
-            window.location.replace('http://127.0.0.1:8000/offers/finished/1?search=' + search + '&sort=' + sort);
+            search = document.querySelector('#searchs').value;
+            window.location.replace(site_url + 'offers/finished/1?search=' + search + '&sort=' + sort);
         }else{
-            search = document.querySelector('#searchtxt').value;
-            window.location.replace('http://127.0.0.1:8000/offers/search/1?search=' + search + '&sort=' + sort);
+            search = document.querySelector('#searchs').value;
+            window.location.replace(site_url + 'offers/search/1?search=' + search + '&sort=' + sort);
             }
         }
     else{
         if(finished != -1){
-            window.location.replace('http://127.0.0.1:8000/offers/finished/1?search=' + miasto + '&sort=' + sort);
+            window.location.replace(site_url + 'offers/finished/1?search=' + miasto + '&sort=' + sort);
         }else{
-            window.location.replace('http://127.0.0.1:8000/offers/search/1?search=' + miasto + '&sort=' + sort);
+            window.location.replace(site_url + 'offers/search/1?search=' + miasto + '&sort=' + sort);
         }
     }
 }
